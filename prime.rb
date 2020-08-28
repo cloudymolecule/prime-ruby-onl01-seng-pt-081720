93357eed219e0262e3470f36e99ed21a16c1ba86
Dario Carlino <baires82@gmail.com>
2020-08-28 00:19:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(integer)
   if integer >= 2
     array = (2..(integer - 1)).to_a
+<<<<<<< HEAD
     array.each do |number|
       if integer % number == 0
         return false
@@ -10,4 +11,16 @@ def prime?(integer)
     return false
   end
   true
+=======
+    array.collect do |number|
+      if integer % number == 0
+        false
+      else
+        true
+      end
+    end
+  elsif integer <= 0 || integer == 1
+    false
+  end
+>>>>>>> f95b20c36b0b7e35db574ea2ea2c0edb058bb97d
 end
